a0bf5910582feab10acd6fd5aaa5c29af10d685d
Terrence <teelt@ymail.com>
2022-10-31 21:11:59 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import ssl
+from flask import Flask, render_template
+import random, requests, json
+
+app = Flask(__name__)
+
+# function to create a list of urls from catsaas.com
+def get_cats(r):
+   cats = []
+   data = json.loads(r.text)
+   for cat in data:
+     cats.append("https://cataas.com/cat/" + cat['_id'])
+   return cats
+
+# get list of cat images from cataas.com
+r = requests.get('https://cataas.com/api/cats?limit=25', verify=False)
+
+# call function to create list of cat urls
+images = get_cats(r)
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
